e8b3ac7cef87dfe261f4335152c8c343eab6468e
mikykonst <mikykonst66@gmail.com>
2017-03-20 15:43:41 +0200

    добавил кухню

---
+++ src/main/java/kitchenService.java
@@ -0,0 +1,11 @@
+package main.java;
+
+import java.util.List;
+
+/**
+ * Created by Евгений on 20.03.2017.
+ */
+public interface kitchenService {
+    void showOrder(List<Dish> dishes);
+    void finishOrder(long id);
+}
