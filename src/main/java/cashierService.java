c5f7cbaaff0e2cc66a441eff8d2ec99fba6669de
mikykonst <mikykonst66@gmail.com>
2017-03-20 15:23:56 +0200

    начал создавать сервис кассира

---
+++ src/main/java/cashierService.java
@@ -0,0 +1,16 @@
+package main.java;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Евгений on 20.03.2017.
+ */
+public interface cashierService {
+    List<Dish> getAllResipts();
+    List<Dish> getResiptById(long id);
+    void deleteItemFromResipt(long id);
+    void clearResipt(long id);
+    void addItemToResipt(Dish dish);
+    void findResiptByDate(Date date);
+}
