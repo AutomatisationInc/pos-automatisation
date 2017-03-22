b3da74c66395c29925cba531d65cffbacc964bf3
Kostya Musienko <kmusienko97@gmail.com>
2017-03-22 18:15:14 +0200

    New project structure

--- src/main/java/ua/automatisationInc/pos/services/KitchenService.java
+++ src/main/java/ua/automatisationInc/pos/services/KitchenService.java
@@ -3,7 +3,7 @@ package ua.automatisationInc.pos.services;
 import ua.automatisationInc.pos.models.Bill;
 
 /**
- * Created by Евгений on 20.03.2017.
+ * Created by Kostya on 20.03.2017.
  */
 public interface KitchenService {
     Bill showOrder(long id);
